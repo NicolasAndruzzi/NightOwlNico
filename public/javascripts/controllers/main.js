2faf68faf2842cbcce270303e49127079d3292cc
Nicolas Andruzzi <NicolasAndruzzi@gmail.com>
2016-05-27 05:42:07 -0600

    nav menu responsive on laptop and tablet widths

--- public/javascripts/controllers/main.js
+++ public/javascripts/controllers/main.js
@@ -141,42 +141,52 @@ app.controller("mainController", function($scope){
     $( this ).addClass( "logoLetterAnimate" );
   });
 
+  /* Scroll-Down Button */
+  $(downPointerIcon).click(function () {
+    $("html, body").animate({
+      scrollTop: $(window).height()
+    }, 1000);
+    return false;
+  });
+
   // Sticky Header
   $(window).scroll(function() {
 
       if ($(window).scrollTop() > $(window).height()*.9) {
           $('.main_header').addClass('sticky');
+          // $('.main_header').fadeIn(3000);
       } else {
           $('.main_header').removeClass('sticky');
+          // $('main_header').fadeOut(3000);
       }
   });
 
-  // Mobile Navigation
-  $('.mobile-toggle').click(function() {
-      if ($('.main_header').hasClass('open-nav')) {
-          $('.main_header').removeClass('open-nav');
-      } else {
-          $('.main_header').addClass('open-nav');
-      }
-  });
-
-  $('.main_header li a').click(function() {
-      if ($('.main_header').hasClass('open-nav')) {
-          $('.navigation').removeClass('open-nav');
-          $('.main_header').removeClass('open-nav');
-      }
-  });
-
-  // navigation scroll
-  $('nav a').click(function(event) {
-      var id = $(this).attr("href");
-      var offset = 0;
-      var target = $(id).offset().top - offset;
-      $('html, body').animate({
-          scrollTop: target
-      }, 500);
-      event.preventDefault();
-  });
+  // // Mobile Navigation
+  // $('.mobile-toggle').click(function() {
+  //     if ($('.main_header').hasClass('open-nav')) {
+  //         $('.main_header').removeClass('open-nav');
+  //     } else {
+  //         $('.main_header').addClass('open-nav');
+  //     }
+  // });
+  //
+  // $('.main_header li a').click(function() {
+  //     if ($('.main_header').hasClass('open-nav')) {
+  //         $('.navigation').removeClass('open-nav');
+  //         $('.main_header').removeClass('open-nav');
+  //     }
+  // });
+  //
+  // // navigation scroll
+  // $('nav a').click(function(event) {
+  //     var id = $(this).attr("href");
+  //     var offset = 0;
+  //     var target = $(id).offset().top - offset;
+  //     $('html, body').animate({
+  //         scrollTop: target
+  //     }, 500);
+  //     event.preventDefault();
+  // });
 
   /* Scroll-to-Top Button */
   $(window).scroll(function () {
@@ -194,36 +204,28 @@ app.controller("mainController", function($scope){
       return false;
   });
 
-  /* Scroll-Down Button */
-  $(downPointerIcon).click(function () {
-      $("html, body").animate({
-          scrollTop: $(window).height()
-      }, 1000);
-      return false;
-  });
-
   /* WORK IN PROGRESS
      NAVIGATION ACTIVE STATE IN SECTION AREA
   */
 
-  var sections = $('section'), nav = $('nav'), nav_height = nav.outerHeight();
-
-  $(window).on('scroll', function () {
-    var cur_pos = $(this).scrollTop();
-
-    sections.each(function() {
-      var top = $(this).offset().top - nav_height,
-          bottom = top + $(this).outerHeight();
-
-      if (cur_pos >= top && cur_pos <= bottom) {
-        nav.find('a').removeClass('active');
-        sections.removeClass('active');
-
-        $(this).addClass('active');
-        nav.find('a[href="#'+$(this).attr('id')+'"]').addClass('active');
-      }
-    });
-  });
+  // var sections = $('section'), nav = $('nav'), nav_height = nav.outerHeight();
+  //
+  // $(window).on('scroll', function () {
+  //   var cur_pos = $(this).scrollTop();
+  //
+  //   sections.each(function() {
+  //     var top = $(this).offset().top - nav_height,
+  //         bottom = top + $(this).outerHeight();
+  //
+  //     if (cur_pos >= top && cur_pos <= bottom) {
+  //       nav.find('a').removeClass('active');
+  //       sections.removeClass('active');
+  //
+  //       $(this).addClass('active');
+  //       nav.find('a[href="#'+$(this).attr('id')+'"]').addClass('active');
+  //     }
+  //   });
+  // });
 
 
 });
